3750015bf38f32fb6ebdb9eeea6e3be6b24aff67
Damon Shetty <damon_53@hotmail.com>
2018-11-23 16:12:31 +0000

    Fail first test then pass by creating API prject and controller.

---
+++ SpeakerMeet.Services.Tests/SpeakerControllerSearchTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using SpeakerMeet;
+using SpeakerMeet.API.Controllers;
+
+namespace SpeakerMeet.Services.Tests
+{
+    public class SpeakerControllerSearchTests
+    {
+        
+        [Fact]
+        public void ItExists()
+        {
+            var controller = new SpeakerMeetController();
+        }
+
+
+    }
+}
